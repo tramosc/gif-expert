6169151d100e35633cdd8fb5e7526460ee416cb7
tramosc <toni.ramos@tecsup.edu.pe>
2020-09-07 23:28:05 -0500

    docs

---
+++ docs/precache-manifest.da2c909f7d8df57f699a0102cf5e09bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1b1643f3aba111888b0840058aab71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9ab81c26922726707d",
+    "url": "/static/css/main.e900f388.chunk.css"
+  },
+  {
+    "revision": "be20dcc49a49de1fd53e",
+    "url": "/static/js/2.b78f2675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b78f2675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9ab81c26922726707d",
+    "url": "/static/js/main.f8f7f274.chunk.js"
+  },
+  {
+    "revision": "eb13ab8655eb2d80e872",
+    "url": "/static/js/runtime-main.0172e228.js"
+  }
+]);
